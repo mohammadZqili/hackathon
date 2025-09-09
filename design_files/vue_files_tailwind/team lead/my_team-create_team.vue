f96c762d7787cd325df82361bdafd90dfdd59972
moh <ameenzqili@gmail.com>
2025-09-09 22:47:13 +0300

    move design files

---
+++ design_files/vue_files_tailwind/team lead/my_team-create_team.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="w-full relative bg-white flex flex-col items-start justify-start text-left text-base text-gray font-space-grotesk">
+        <div class="self-stretch bg-mintcream overflow-hidden flex flex-col items-start justify-start min-h-[800px]">
+            <div class="self-stretch flex flex-col items-start justify-start">
+                <div class="self-stretch flex-1 flex flex-row items-start justify-center py-5 px-40">
+                    <div class="w-[960px] h-[760px] overflow-hidden shrink-0 flex flex-col items-start justify-start py-5 px-0 box-border max-w-[960px]">
+                        <div class="self-stretch flex flex-row items-start justify-between flex-wrap content-start p-4 text-[32px]">
+                            <div class="w-72 flex flex-col items-start justify-start min-w-[288px]">
+                                <b class="self-stretch relative leading-10">Create Team</b>
+                            </div>
+                        </div>
+                        <div class="flex flex-row items-end justify-start flex-wrap content-end py-3 px-4 box-border max-w-[480px] text-seagreen">
+                            <div class="flex-1 flex flex-col items-start justify-start min-w-[160px]">
+                                <div class="self-stretch rounded-xl bg-mintcream border-honeydew border-solid border-[1px] box-border h-14 overflow-hidden shrink-0 flex flex-row items-center justify-start p-[15px]">
+                                    <div class="w-[190px] relative leading-6 whitespace-pre-wrap inline-block shrink-0">Team  Name</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex flex-row items-end justify-start flex-wrap content-end py-3 px-4 box-border max-w-[480px]">
+                            <div class="flex-1 flex flex-col items-start justify-start min-w-[160px]">
+                                <div class="self-stretch rounded-xl bg-mintcream border-honeydew border-solid border-[1px] box-border h-14 overflow-hidden shrink-0 flex flex-row items-center justify-between relative gap-0">
+                                    <img class="w-[11px] absolute !!m-[0 important] top-[17px] left-[424px] h-[21px] object-contain z-[0]" alt="" src="Vector - 0.svg" />
+                                    <div class="w-[394px] relative leading-6 whitespace-pre-wrap inline-block shrink-0 z-[1]">Hackathon  Edition</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex flex-row items-end justify-start flex-wrap content-end py-3 px-4 box-border max-w-[480px] text-seagreen">
+                            <div class="flex-1 flex flex-col items-start justify-start min-w-[160px]">
+                                <div class="self-stretch rounded-xl bg-mintcream border-honeydew border-solid border-[1px] box-border h-14 overflow-hidden shrink-0 flex flex-row items-center justify-start p-[15px]">
+                                    <div class="w-[395px] relative leading-6 whitespace-pre-wrap inline-block shrink-0">Invite  Members (Email)</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="self-stretch h-[47px] flex flex-col items-start justify-start pt-4 px-4 pb-2 box-border text-lg">
+                            <b class="self-stretch relative leading-[23px]">Invited Members</b>
+                        </div>
+                        <div class="self-stretch bg-mintcream h-14 flex flex-row items-center justify-between py-0 px-4 box-border gap-0 min-h-[56px]">
+                            <div class="flex-1 overflow-hidden flex flex-col items-start justify-start">
+                                <div class="self-stretch relative leading-6 overflow-hidden text-ellipsis whitespace-nowrap">Member 1</div>
+                            </div>
+                            <img class="w-7 h-7" alt="" src="Depth 5, Frame 1.svg" />
+                        </div>
+                        <div class="self-stretch bg-mintcream h-14 flex flex-row items-center justify-between py-0 px-4 box-border gap-0 min-h-[56px]">
+                            <div class="flex-1 overflow-hidden flex flex-col items-start justify-start">
+                                <div class="self-stretch relative leading-6 overflow-hidden text-ellipsis whitespace-nowrap">Member 2</div>
+                            </div>
+                            <img class="w-7 h-7" alt="" src="Depth 5, Frame 1.svg" />
+                        </div>
+                        <div class="self-stretch bg-mintcream h-14 flex flex-row items-center justify-between py-0 px-4 box-border gap-0 min-h-[56px]">
+                            <div class="flex-1 overflow-hidden flex flex-col items-start justify-start">
+                                <div class="self-stretch relative leading-6 overflow-hidden text-ellipsis whitespace-nowrap">Member 3</div>
+                            </div>
+                            <img class="w-7 h-7" alt="" src="Depth 5, Frame 1.svg" />
+                        </div>
+                        <div class="self-stretch flex flex-row items-start justify-start py-3 px-4 text-center text-sm">
+                            <div class="w-[480px] rounded-xl bg-mediumseagreen h-10 overflow-hidden flex flex-row items-center justify-center py-0 px-4 box-border min-w-[84px] max-w-[480px]">
+                                <div class="overflow-hidden flex flex-col items-center justify-start">
+                                    <b class="self-stretch relative leading-[21px] overflow-hidden text-ellipsis whitespace-nowrap">Create Team</b>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ design_files/vue_files_tailwind/team member/Register.vue
@@ -0,0 +1,123 @@
+<template>
+    <div class="w-full relative bg-mintcream-100 h-[800px] text-center text-[45px] text-mintcream-100 font-ruman-one">
+        <img class="absolute top-[0px] left-[0px] w-[1355px] h-[1024px] object-cover" alt="" src="Noise & Texture.png" />
+        <div class="absolute top-[0px] left-[0px] bg-seagreen w-[424px] h-[800px]" />
+        <div class="absolute top-[276px] left-[57px] flex flex-col items-start justify-start pt-5 px-4 pb-3 gap-4">
+            <b class="self-stretch relative leading-[51px]">Welcome Back!</b>
+            <div class="w-[277px] relative text-base leading-6 text-left inline-block">If you Already Have An Account, Please Sign in</div>
+            <div class="self-stretch h-[88px] flex flex-row items-end justify-center py-3 px-4 box-border text-sm">
+                <div class="w-[205px] rounded-xl bg-mediumseagreen border-mintcream-100 border-solid border-[1px] box-border h-10 overflow-hidden shrink-0 flex flex-row items-center justify-center py-0 px-4 min-w-[84px] max-w-[480px]">
+                    <div class="overflow-hidden flex flex-col items-center justify-start">
+                        <b class="self-stretch relative leading-[21px] overflow-hidden text-ellipsis whitespace-nowrap">Login</b>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="absolute top-[calc(50%_-_325.44px)] left-[529px] w-[634px] overflow-hidden flex flex-col items-start justify-start max-w-[670.31px] text-left text-[11.17px] text-seagreen font-space-grotesk">
+            <div class="self-stretch flex flex-col items-start justify-start pt-3.5 px-[11.2px] pb-[8.4px] text-[19.55px] text-gray-100">
+                <b class="self-stretch relative leading-[24.44px]">Register</b>
+            </div>
+            <div class="self-stretch flex flex-row items-start justify-start p-[11px] gap-2 text-[9.78px] text-gray-100">
+                <div class="flex-1 rounded-[8.38px] border-honeydew border-solid border-[0.7px] box-border h-[37px] flex flex-row items-center justify-start p-[10.5px] gap-[11.2px]">
+                    <img class="w-3.5 relative rounded-[6.98px] h-3.5 overflow-hidden shrink-0" alt="" src="Depth 6, Frame 0.svg" />
+                    <div class="flex flex-col items-start justify-start">
+                        <div class="w-[564.2px] flex-1 flex flex-col items-start justify-start">
+                            <div class="self-stretch relative leading-[14.66px] font-medium">Team Leader</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex-1 rounded-[8.38px] border-honeydew border-solid border-[0.7px] box-border h-[37px] flex flex-row items-center justify-start p-[10.5px] gap-[11.2px]">
+                    <div class="w-3.5 rounded-[6.98px] border-honeydew border-solid border-[1.4px] box-border h-3.5" />
+                    <div class="flex flex-col items-start justify-start">
+                        <div class="w-[564.2px] flex-1 flex flex-col items-start justify-start">
+                            <div class="self-stretch relative leading-[14.66px] font-medium">Team Member</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex-1 rounded-[8.38px] border-honeydew border-solid border-[0.7px] box-border h-[37px] flex flex-row items-center justify-start p-[10.5px] gap-[11.2px]">
+                    <div class="w-3.5 rounded-[6.98px] border-honeydew border-solid border-[1.4px] box-border h-3.5" />
+                    <div class="flex flex-col items-start justify-start">
+                        <div class="w-[564.2px] flex-1 flex flex-col items-start justify-start">
+                            <div class="self-stretch relative leading-[14.66px] font-medium">Visitor</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-row items-end justify-start flex-wrap content-end py-[8.4px] px-[11.2px] box-border max-w-[335.15px]">
+                <div class="flex-1 flex flex-col items-start justify-start min-w-[111.72px]">
+                    <div class="self-stretch rounded-[8.38px] bg-mintcream-200 h-[39.1px] overflow-hidden shrink-0 flex flex-row items-center justify-start p-[11.2px] box-border">
+                        <div class="relative leading-[16.76px]">Name</div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-row items-end justify-start flex-wrap content-end py-[8.4px] px-[11.2px] box-border max-w-[335.15px]">
+                <div class="flex-1 flex flex-col items-start justify-start min-w-[111.72px]">
+                    <div class="self-stretch rounded-[8.38px] bg-mintcream-200 h-[39.1px] overflow-hidden shrink-0 flex flex-row items-center justify-start p-[11.2px] box-border">
+                        <div class="relative leading-[16.76px]">Email</div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-row items-end justify-start flex-wrap content-end py-[8.4px] px-[11.2px] box-border max-w-[335.15px]">
+                <div class="flex-1 flex flex-col items-start justify-start min-w-[111.72px]">
+                    <div class="self-stretch rounded-[8.38px] bg-mintcream-200 h-[39.1px] overflow-hidden shrink-0 flex flex-row items-center justify-start p-[11.2px] box-border">
+                        <div class="relative leading-[16.76px] whitespace-pre-wrap">National  ID</div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-row items-end justify-start flex-wrap content-end py-[8.4px] px-[11.2px] box-border max-w-[335.15px]">
+                <div class="flex-1 flex flex-col items-start justify-start min-w-[111.72px]">
+                    <div class="self-stretch rounded-[8.38px] bg-mintcream-200 h-[39.1px] overflow-hidden shrink-0 flex flex-row items-center justify-start p-[11.2px] box-border">
+                        <div class="relative leading-[16.76px]">Mobile Number</div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-row items-end justify-start flex-wrap content-end py-[8.4px] px-[11.2px] box-border max-w-[335.15px]">
+                <div class="flex-1 flex flex-col items-start justify-start min-w-[111.72px]">
+                    <div class="self-stretch rounded-[8.38px] bg-mintcream-200 h-[39.1px] overflow-hidden shrink-0 flex flex-row items-center justify-start p-[11.2px] box-border">
+                        <div class="relative leading-[16.76px]">Password</div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-row items-end justify-start flex-wrap content-end py-[8.4px] px-[11.2px] box-border max-w-[335.15px]">
+                <div class="flex-1 flex flex-col items-start justify-start min-w-[111.72px]">
+                    <div class="self-stretch rounded-[8.38px] bg-mintcream-200 h-[39.1px] overflow-hidden shrink-0 flex flex-row items-center justify-start p-[11.2px] box-border">
+                        <div class="relative leading-[16.76px]">Confirm Password</div>
+                    </div>
+                </div>
+            </div>
+            <div class="self-stretch relative text-base leading-6 font-medium text-gray-200">Occupation</div>
+            <div class="self-stretch flex flex-row items-start justify-start p-[11px] gap-2 text-[9.78px] text-gray-100">
+                <div class="flex-1 rounded-[8.38px] border-honeydew border-solid border-[0.7px] box-border h-[37px] flex flex-row items-center justify-start p-[10.5px] gap-[11.2px]">
+                    <img class="w-3.5 relative rounded-[6.98px] h-3.5 overflow-hidden shrink-0" alt="" src="Depth 6, Frame 0.svg" />
+                    <div class="flex flex-col items-start justify-start">
+                        <div class="w-[564.2px] flex-1 flex flex-col items-start justify-start">
+                            <div class="self-stretch relative leading-[14.66px] font-medium">Student</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex-1 rounded-[8.38px] border-honeydew border-solid border-[0.7px] box-border h-[37px] flex flex-row items-center justify-start p-[10.5px] gap-[11.2px]">
+                    <div class="w-3.5 rounded-[6.98px] border-honeydew border-solid border-[1.4px] box-border h-3.5" />
+                    <div class="flex flex-col items-start justify-start">
+                        <div class="w-[564.2px] flex-1 flex flex-col items-start justify-start">
+                            <div class="self-stretch relative leading-[14.66px] font-medium">Employee</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-row items-end justify-start flex-wrap content-end py-[8.4px] px-[11.2px] box-border max-w-[335.15px]">
+                <div class="flex-1 flex flex-col items-start justify-start min-w-[111.72px]">
+                    <div class="self-stretch rounded-[8.38px] bg-mintcream-200 h-[39.1px] overflow-hidden shrink-0 flex flex-row items-center justify-start p-[11.2px] box-border">
+                        <div class="relative leading-[16.76px]">Job title</div>
+                    </div>
+                </div>
+            </div>
+            <div class="self-stretch flex flex-row items-center justify-end py-4 px-[11.2px] text-center text-base text-mintcream-100 font-ruman-one">
+                <div class="w-[226px] rounded-xl [background:linear-gradient(rgba(79,_150,_115,_0.75),_rgba(79,_150,_115,_0.75)),_#4f9673] flex flex-row items-center justify-center py-3 px-4 box-border max-h-[40px]">
+                    <b class="relative leading-[14.66px]">Register</b>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+</script>
